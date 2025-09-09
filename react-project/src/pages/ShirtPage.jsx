b81841c3fbe37d118cb9d7dc9d921fd93cf8356f
MrKubis <jakub.f102@gmail.com>
2025-09-09 11:06:43 +0200

    init:Added page container for shirtpage

--- react-project/src/pages/ShirtPage.jsx
+++ react-project/src/pages/ShirtPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import "./styles/ShirtPage.css"
 
 function ShirtPage(){
+    console.log(order)
     const params = useParams();
     const [shirt, setShirt] = useState(null)
     //Setting shirt
@@ -23,13 +24,94 @@ function ShirtPage(){
         )
     },[])
     if(shirt)
-    console.log(shirt)
+        //I deffinetely need to fix this one
     return(
         <div>
             {shirt && 
-            <div className="div-sdaspage-container">
-                <p>{shirt.name}</p>
-                <img src={shirt.image}/>
+            <div className="page-container">
+                
+                <div className="image-container">
+                    <img src={shirt.image}/>
+                    <img src={shirt.image}/>
+                </div>
+                <div className="text-container">
+                    <p className="text-name">{shirt.name}</p>
+                   
+                    <text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</text>
+                </div>    
+                
+                 <div className="scroll-container">
+                        <ul aria-hidden="true">
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                            <li>
+                                <p className="text-price">{shirt.price}zł</p>
+                            </li>
+                        </ul>
+                    </div>
             </div>
             }
         </div>
