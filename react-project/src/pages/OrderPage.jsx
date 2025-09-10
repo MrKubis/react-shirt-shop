b98593f8ec6c1f905574fa2dd052aeb0734c8153
MrKubis <jakub.f102@gmail.com>
2025-09-10 21:30:42 +0200

    Init: Mapping ordered items
    Mapping ordered items and calculating total sum

--- react-project/src/pages/OrderPage.jsx
+++ react-project/src/pages/OrderPage.jsx
@@ -1,10 +1,28 @@
 
-function OrderPage(){
+import { useContext, useState } from "react";
+import { OrderContext } from "../contexts/OrderContext.jsx";
 
+function OrderPage(){
+    const {order, clearOrder} = useContext(OrderContext)
+    let total = 0
     return(
         <div>
+           
             <h1>Welcome to the order page!!!</h1>
             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+             <ul>
+                {order.map((item) => {
+                    total += item.quantity * item.price
+                    return(
+                        <p>
+                            {item.name} x {item.quantity}                            
+                        </p>
+                    );
+
+                       
+                })}
+            </ul>
+            <p>Total:{total}</p>
         </div>
     );
 }
