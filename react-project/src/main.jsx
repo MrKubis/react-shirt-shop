5554e4e67a32f9022bc446500fbf8891bea4d9ef
MrKubis <jakub.f102@gmail.com>
2025-09-12 15:36:36 +0200

    feat: Added Layout to structure better

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from  'react-router-dom';
+import { createBrowserRouter, RouterProvider,useLocation } from  'react-router-dom';
 
 import './index.css'
+
+import { AnimatePresence } from "framer-motion"
 
 import HomePage from './pages/HomePage.jsx';
 import OrderPage from './pages/OrderPage.jsx'
@@ -14,59 +16,29 @@ import ShirtsPage from './pages/ShirtsPage.jsx';
 import ShirtPage from './pages/ShirtPage.jsx';
 
 import { OrderContextProvider,OrderContext } from './contexts/OrderContext.jsx';
+import Layout from './pages/Layout.jsx';
 
 const router = createBrowserRouter([
+  
   {
     path: '/',
-    element:<>
-    <NavBar />
-    <HomePage />
-    </> ,
-    errorElement: <NotFoundPage/>
-  },
-  {
-    path: '/order/',
-    element : <>
-    <NavBar />
-    <OrderPage />
-    </> ,
-  },
-  {
-    path: '/shirts/',
-    element:<>
-    <NavBar/>
-    <ShirtsPage/>
-    </>,
-  },  
-  {
-    path:'/shirts/:shirtName',
-    element:<>
-    <NavBar/>
-    <ShirtPage/>
-    </>
-  },
-  {
-    path: '/profiles/',
-    element: <>
-    <NavBar />
-    <ProfilesPage />
-    </> ,
+    element: <Layout />,
+    errorElement: <NotFoundPage/>,
     children:[
-        {
-          path:'/profiles/:profileId', 
-          element: <ProfilePage/>
-        },
+      {index : true, element: <HomePage/>},
+      {path: "order", element: <OrderPage/>},
+      {path:"shirts", element: <ShirtsPage/>},
+      {path:"shirts/:shirtName", element: <ShirtPage/>}
     ]
-  },
+  }
 ]
 );
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <OrderContextProvider>
-      <RouterProvider router={router}>
-      </RouterProvider>
+        <RouterProvider router={router}>
+        </RouterProvider>
     </OrderContextProvider>
   </StrictMode>,
 )
---
+++ react-project/src/pages/Layout.jsx
@@ -0,0 +1,34 @@
+import {Outlet,useLocation} from "react-router-dom"
+import { AnimatePresence,motion } from "framer-motion"
+import NavBar from "../Navbar.jsx"
+import { ToastContainer, toast } from 'react-toastify';
+
+function Layout(){
+    
+    const location = useLocation()
+    return(
+        <>
+        <NavBar />
+        <AnimatePresence mode = "wait" initial={false}>
+            <motion.div
+            key = {location.pathname}
+ 
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            style={{opacity:0}}
+            transition={{duration:0.2}}
+            >
+                
+                <Outlet/>
+
+            </motion.div>
+            </AnimatePresence>
+                <ToastContainer
+                position="bottom-right"
+                />
+        </>
+    );
+
+}
+
+export default Layout
