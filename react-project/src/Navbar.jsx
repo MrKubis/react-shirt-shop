3284a4bf9110f2ae18126b7a9790962f6f076aba
MrKubis <jakub.f102@gmail.com>
2025-09-12 15:36:50 +0200

    feat: Navbar improvement

--- react-project/src/Navbar.jsx
+++ react-project/src/Navbar.jsx
@@ -2,20 +2,17 @@ import { NavLink } from "react-router-dom";
 import './NavBar.css'
 function NavBar(){
     return (
-        <nav className="nav">
-            <div className="nav-home-container">
-                <NavLink to="/" >Home</NavLink>
+        <div className="navbar-container">
+            <div className="nav-route">
+                <NavLink to="/" viewTransition>Home</NavLink>
             </div>
-
-            <div className="nav-routes-container">
-                <div className="nav-route">
-                    <NavLink to="/shirts" >Shirts</NavLink>
-                </div>
-                <div className="nav-route">
-                    <NavLink to = "/order">Your order</NavLink>
-                </div>
+            <div className="nav-route">
+                <NavLink to="/shirts" viewTransition>Shirts</NavLink>
             </div>
-        </nav>
+            <div className="nav-route">
+                <NavLink to = "/order" viewTransition>Your order</NavLink>
+            </div>
+        </div>
     );
 }
 
