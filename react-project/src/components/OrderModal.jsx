7ef919cbb71bd681ff106a1a2d8d8d71cd9833ee
MrKubis <jakub.f102@gmail.com>
2025-09-18 22:10:43 +0200

    init:Docker file and some other stuff idk anymore xddddd

---
+++ react-project/src/components/OrderModal.jsx
@@ -0,0 +1,28 @@
+import { useForm } from 'react-hook-form'
+
+type FormFields = {
+    name:string;
+    email:string;
+}
+import "./OrderModal.css"
+export default function OrderModal({toggleModal}){
+    const form = useForm();
+    return(
+        <div className="modal">
+                    <div className="overlay"/>
+
+                    <div className="modal-content">
+                        <button className="modal-content-button"
+                        onClick={toggleModal}
+                        >
+                            Close
+                        </button>
+                        <p>Fill out your data:</p>
+                    <form> 
+                        <input type="text" placeholder="Your name..."></input>
+                        <input type="text" placeholder="Your e-mail address..."></input>
+                    </form>
+                    </div>
+                </div>
+    );
+}
